41a079a6b99f69ccfcf6dad7f9be85c6b0a9da1f
Stefan Kubinski <stefan.kubinski@gmail.com>
2024-05-08 07:21:54 +0200

    used efcore with npgsql

--- logbook.api/Logbook.API/DataContext.cs
+++ logbook.api/Logbook.API/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.JSInterop.Infrastructure;
 using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Logbook.API
 {
@@ -15,9 +17,19 @@ namespace Logbook.API
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             // in memory database used for simplicity, change to a real db for production applications
-            options.UseInMemoryDatabase("Logbook");
-        }
+            //options.UseInMemoryDatabase("Logbook");
+            options.UseNpgsql("User id=admin;Password=postgres;Host=localhost;Port=5532;Database=postgres");
 
+            //Migration erstellen:
+            //dotnet-ef migrations add <Name>
+
+            //Migration auf DB schreiben:    
+            //dotnet-ef database update
+
+            //dotnet ef database update 0
+            //dotnet ef migrations remove
+        }
         public DbSet<Aircraft> Aircraft { get; set; }
+        public DbSet<Hangar> Hangar { get; set; }
     }
 }
---
+++ logbook.api/Logbook.API/Migrations/20240507172920_addedHangar.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Logbook.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedHangar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HangarId",
+                table: "Aircraft",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Hangar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hangar", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aircraft_HangarId",
+                table: "Aircraft",
+                column: "HangarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Aircraft_Hangar_HangarId",
+                table: "Aircraft",
+                column: "HangarId",
+                principalTable: "Hangar",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Aircraft_Hangar_HangarId",
+                table: "Aircraft");
+
+            migrationBuilder.DropTable(
+                name: "Hangar");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Aircraft_HangarId",
+                table: "Aircraft");
+
+            migrationBuilder.DropColumn(
+                name: "HangarId",
+                table: "Aircraft");
+        }
+    }
+}
